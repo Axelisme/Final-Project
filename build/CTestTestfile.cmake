922e0160a94f0fd064a5c86da5d84218be587c9b
Axel_Chiu <jary33333@gapp.nthu.edu.tw>
2022-12-31 22:21:58 +0800

    test

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/axel/Documents/VSCode/Final Project/src
-# Build directory: /home/axel/Documents/VSCode/Final Project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
